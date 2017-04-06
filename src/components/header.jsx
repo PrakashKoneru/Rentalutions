229a5917d56a25d21a0fec7809eacd7031bb7d67
PrakashKoneru <PrakashKoneru@Sivas-MBP.T-mobile.com>
2017-04-05 22:39:53 -0400

    Final changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ module.exports= React.createClass({
     render : function() {
         return  <div className = "headerComponent text-center">
         			<div>
-        				<img src = './images/logo.svg'/>
+        				<img src = '../../images/logo.svg'/>
         			</div>
                 </div>
     }
